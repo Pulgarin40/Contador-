356d8126c076fafcdb4142d2624522c9e6b31e95
GabrielH00234 <humaneshugo123@gmail.com>
2024-10-02 16:46:06 +0200

    Add files via upload

---
+++ contador/contador.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+ 
+  import math
+
+numero = 16
+raiz_cuadrada = math.sqrt(numero)
+print(raiz_cuadrada)  # Salida: 4.0
+
+}
