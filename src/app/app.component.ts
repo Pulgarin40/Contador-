e65a5154d1e09a02441fa64d3d2c2bc28d29a24d
JPablo <jberpu82@gmail.com>
2024-10-04 09:15:19 +0200

    Finalización de App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';  
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FormsModule],  
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class AppComponent {
-  title = 'mi-proyecto-angular';
-  counter: number = 0;  // Inicializamos el contador en 0
-  inputValue: number = 0;  // Valor por defecto del input
+  title = 'Contador';
+  counter: number = 0;
+  inputValue: number = 0;
 
-  // Método para incrementar el contador
   increment() {
     this.counter++;
   }
 
-  // Método para decrementar el contador
   decrement() {
     this.counter--;
   }
 
-  // Método para calcular la raíz cuadrada
   calculateSqrt() {
     if (this.counter >= 0) {
       this.counter = Math.sqrt(this.counter);
@@ -33,7 +29,6 @@ export class AppComponent {
     }
   }
 
-  // Método para actualizar el contador con el valor ingresado
   updateCounter(value: string) {
     const parsedValue = parseInt(value, 10);
     if (!isNaN(parsedValue)) {
